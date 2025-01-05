fe264f9343fa548d70f37b15b846fc9daef5df5f
Krantikumar _More <kumarmore1266@gmail.com>
2025-01-05 19:31:22 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+//1. Real-World Problems Solved Using Programming
+
+// E-commerce Applications: Automating inventory management and order processing.
+// Weather Forecasting: Using data analysis to predict weather patterns.
+
+//2. Difference Between final and const in Dart
+// final: A variable declared with final is initialized only once and can hold a runtime constant value.
+// const: A variable declared with const is a compile-time constant and must be assigned a value at compile time.
+
+
+//3. AREA OF CIRCLE
+// double calculateArea(double radius) {
+//   return 3.14159 * radius * radius;
+// }
+//
+// void main() {
+//   double radius = 5.0;
+//   double area = calculateArea(radius);
+//   print('The area of the circle with radius $radius is $area.');
+// }
+
+//4.Difference Between for and while Loops and When to Use Them
+
+// for Loop: Used when the number of iterations is known beforehand.
+// while Loop: Used when the number of iterations is not known and depends on a condition.
+
+
+
